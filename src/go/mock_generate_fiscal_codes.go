29ec636e14db6630bfb0804d8be646df323eb730
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-04-08 12:20:54 +0200

    mock_generator: clean code

--- src/go/mock_generate_fiscal_codes.go
+++ src/go/mock_generate_fiscal_codes.go
@@ -11,30 +11,31 @@ import (
 	"strings"
 )
 
-func main()  {
+func main() {
 
 	var listCodiciFiscali []string
 
-	fileCsv := generateCsvFile("mock_cf.csv")
-
 	for i := 0; i < 10; i++ {
-		listCodiciFiscali = append(listCodiciFiscali, generateMockCodiceFiscale(i) ) 
+		listCodiciFiscali = append(listCodiciFiscali, generateMockCodiceFiscale(i))
 	}
-	fmt.Println(listCodiciFiscali)
-
-	csvWriter := csv.NewWriter(fileCsv)
-
-	for _, empRow := range listCodiciFiscali {
-		println(empRow)
-		_ = csvWriter.Write(strings.Fields(empRow))
-	}
-	
-	csvWriter.Flush()
-	fileCsv.Close()
+	writeCodiciFiscaleInCsv(listCodiciFiscali)
 
 }
 
-func generateCsvFile(filename string) (*os.File)  {
+func writeCodiciFiscaleInCsv(listCodiciFiscali []string) {
+	fileCsv := generateCsvFile("mock_cf.csv")
+	csvWriter := csv.NewWriter(fileCsv)
+
+	for _, empRow := range listCodiciFiscali {
+		fmt.Println(empRow)
+		_ = csvWriter.Write(strings.Fields(empRow))
+	}
+
+	csvWriter.Flush()
+	fileCsv.Close()
+}
+
+func generateCsvFile(filename string) *os.File {
 	csvFile, err := os.Create(filename)
 
 	if err != nil {
@@ -43,15 +44,15 @@ func generateCsvFile(filename string) (*os.File)  {
 	return csvFile
 }
 
-func generateMockCodiceFiscale(randomNum int) string{
-	
-	var rawHash string = getMD5Hash( strconv.Itoa(randomNum))
+func generateMockCodiceFiscale(randomNum int) string {
+
+	var rawHash string = getMD5Hash(strconv.Itoa(randomNum))
 	codiceFiscaleMock := rawHash[:16]
 
 	return codiceFiscaleMock
 }
 
 func getMD5Hash(text string) string {
-   hash := md5.Sum([]byte(text))
-   return hex.EncodeToString(hash[:])
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
 }
