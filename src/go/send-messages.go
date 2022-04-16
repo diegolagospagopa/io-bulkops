e8cd912c6897cc59fd4f2e4dc9d4d9f7b141456d
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-04-16 18:58:38 +0200

    ready send alpha

--- src/go/send-messages.go
+++ src/go/send-messages.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	// "container/list"
 	"encoding/csv"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
-	readCsvData("mock_cf.csv")
+	channel := make(chan string)
+	var cfList [][]string = readCsvData("mock_cf.csv")
+
+	go func (ch chan string)  {
+		defer func() {
+			fmt.Println("📢 call close")
+			close(ch)
+		}()
+		for i, cf := range cfList {
+			sendCode(cf[0], ch)
+			fmt.Println("cf number ", i, " sended")
+		}
+	}(channel)
+
+
+	for c := range channel {
+		go func (item string)  {
+			fmt.Println("Channel response: ", item)
+		}(c)
+	}
+	fmt.Println("🚀 completed with success")
 }
 
-func sendCode(code string)  {
+func sendCode(code string, channel chan string) {
 	fmt.Println(code)
+
+	var requestUrl = fmt.Sprintf("http://localhost:3000/%s", code)
+
+	_, err := http.Get(requestUrl)
+	if err != nil {
+		fmt.Println(requestUrl, "might be down!")
+		channel <- requestUrl
+		return
+	}
+
+	fmt.Println(requestUrl, "is up!")
+	channel <- requestUrl
 }
 
-func readCsvData(fileName string) {
+func readCsvData(fileName string) [][]string {
 	// open file
 	f, err := os.Open(fileName)
 	if err != nil {
@@ -31,5 +65,7 @@ func readCsvData(fileName string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(data)
+
+	// fmt.Println(data)
+	return data
 }
